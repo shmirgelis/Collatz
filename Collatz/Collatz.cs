97347a573310211196f7be72a04b23bb1c10cd7c
shmirgelis <73270957+shmirgelis@users.noreply.github.com>
2020-10-25 10:57:46 +0200

    Add files via upload

---
+++ Collatz/Collatz.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Collatz
+{
+    class Collatz
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter positive integer");
+            string givenNumber = Console.ReadLine();
+            uint number = uint.Parse(givenNumber); //using uint integer type to calculate 837799, int type range is too short
+            uint i = 0;
+            while(number > 1)
+            {
+                if (number % 2 == 0)
+                {
+                    number = number / 2;
+                }
+                else
+                {
+                    number = number * 3 + 1;
+                }
+                Console.WriteLine(number);
+                i++;
+            }
+            Console.WriteLine("It took " + i + " steps");
+        }
+    }
+}
